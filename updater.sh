dda502ecd6470fbc15506c2a844e0fb6ce03052f
Shi Han NG <shihanng@gmail.com>
2018-05-19 18:32:31 +0900

    rename variable

--- updater.sh
+++ updater.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 NPM_VERSION="$(npm view @bitwarden/cli "dist-tags".latest)"
-DOCKERHUB_VERSION="$(docker run --rm shihanng/bw:latest --version)"
+DOCKERFILE_VERSION="$(grep -oP '(?<=bitwarden/cli@)[0-9.]+' Dockerfile)"
 
-if [ "$NPM_VERSION" != "$DOCKERHUB_VERSION" ]
+if [ "$NPM_VERSION" != "$DOCKERFILE_VERSION" ]
 then
   sed -i "s|\(install -g @bitwarden/cli@\)[0-9\.]\+$|\1${NPM_VERSION}|" Dockerfile
   git add Dockerfile
